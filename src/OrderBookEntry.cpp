e39f92ec57465b941c7ad9e39c99e78e2e6b014b
abdo <mac@gmail.com>
2024-11-22 02:07:38 +0200

    Enhance OrderBookEntry class with move semantics and comparison functions
    - Updated constructor to use move semantics for string members (_timestamp, _product, and _username).
    - Refined stringToOrderBookType method to handle unknown values more clearly.
    - Added comparison functions: compareByTimestamp, compareByPriceAsc, and compareByPriceDesc for better sorting flexibility.

--- src/OrderBookEntry.cpp
+++ src/OrderBookEntry.cpp
@@ -1,4 +1,4 @@
-#include"OrderBookEntry.h"
+#include "OrderBookEntry.h"
 
 OrderBookEntry::OrderBookEntry(
     double _price,
@@ -9,19 +9,38 @@ OrderBookEntry::OrderBookEntry(
     std::string _username)
     : price(_price),
       amount(_amount),
-      timestamp(_timestamp),
-      product(_product),
+      timestamp(std::move(_timestamp)),
+      product(std::move(_product)),
       orderType(_orderType),
-      username(_username)
+      username(std::move(_username))
 {
 }
 
-OrderBookType OrderBookEntry::stringToOrderBookType(std::string s)
+OrderBookType OrderBookEntry::stringToOrderBookType(const std::string& s)
 {
-  if(s == "ask")
-    return OrderBookType::ask;
-  if(s == "bid")
-    return OrderBookType::bid;
-  return OrderBookType::unkown;
+    if (s == "ask") {
+        return OrderBookType::ask;
+    } 
+    else if (s == "bid") {
+        return OrderBookType::bid;
+    } 
+    else
+    {
+      return OrderBookType::unkown; // set it as unkown if it's not bid or ask
+    }
 }
 
+bool OrderBookEntry::compareByTimestamp(const OrderBookEntry& e1, const OrderBookEntry& e2)
+{
+    return e1.timestamp < e2.timestamp;
+}
+
+bool OrderBookEntry::compareByPriceAsc(OrderBookEntry& e1, OrderBookEntry& e2)
+{
+    return e1.price < e2.price;
+}
+
+bool OrderBookEntry::compareByPriceDesc(OrderBookEntry& e1, OrderBookEntry& e2)
+{
+    return e1.price > e2.price;
+}
